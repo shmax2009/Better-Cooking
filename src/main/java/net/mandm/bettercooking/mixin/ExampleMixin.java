4949a2bafe390e77a917f9a792825e4b3fc861d5
maksym <maximschved8@gmail.com>
2022-10-24 15:49:06 +0200

    Setup All

--- src/main/java/net/mandm/bettercooking/mixin/ExampleMixin.java
+++ src/main/java/net/mandm/bettercooking/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package net.mandm.bettercooking.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import net.mandm.bettercooking.BetterCookingMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -9,8 +9,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
-	}
+    @Inject(at = @At("HEAD"), method = "init()V")
+    private void init(CallbackInfo info) {
+        BetterCookingMod.LOGGER.info("This line is printed by an example mod mixin!");
+    }
 }
